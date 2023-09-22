f7379dd1805c4820a033b0b03966c970abb03117
Huu Hoc <nguyenhuuhoc25042003@gmail.com>
2023-09-22 22:26:47 +0700

    Cart Manage(add,upQty,downQty,if you add an existing item then upQty)

--- clone/src/main/java/com/shopee/clone/service/cart/CartService.java
+++ clone/src/main/java/com/shopee/clone/service/cart/CartService.java
@@ -5,4 +5,10 @@ import org.springframework.http.ResponseEntity;
 public interface CartService {
     ResponseEntity<?> addToCart(Long pItemId, Long uId);
     void remove(Long id);
+
+    ResponseEntity<?> increaseQty(Long pItemId);
+
+    ResponseEntity<?> reduceQty(Long cartId);
+
+    ResponseEntity<?> delete(Long cartId);
 }
--- clone/src/main/java/com/shopee/clone/service/cart/impl/CartServiceImpl.java
+++ clone/src/main/java/com/shopee/clone/service/cart/impl/CartServiceImpl.java
@@ -1,18 +1,18 @@
 package com.shopee.clone.service.cart.impl;
 
-import com.shopee.clone.DTO.auth.refresh_token.RefreshTokenResponse;
 import com.shopee.clone.DTO.cart.CartResponse;
-import com.shopee.clone.DTO.product.OptionType;
-import com.shopee.clone.DTO.product.OptionValue;
-import com.shopee.clone.DTO.product.ProductItem;
-import com.shopee.clone.DTO.product.response.*;
-import com.shopee.clone.entity.*;
+import com.shopee.clone.DTO.product.response.OptionTypeDTO;
+import com.shopee.clone.DTO.product.response.OptionValueDTO;
+import com.shopee.clone.DTO.product.response.ProductItemResponseDTO;
+import com.shopee.clone.DTO.product.response.ProductMatchToCartResponse;
+import com.shopee.clone.entity.ProductItemEntity;
+import com.shopee.clone.entity.UserEntity;
 import com.shopee.clone.entity.cart.CartEntity;
 import com.shopee.clone.repository.cart.CartRepository;
 import com.shopee.clone.repository.product.OptionTypeRepository;
-import com.shopee.clone.repository.product.OptionValueRepository;
 import com.shopee.clone.repository.product.ProductItemRepository;
 import com.shopee.clone.service.cart.CartService;
+import com.shopee.clone.service.productItem.impl.ProductItemService;
 import com.shopee.clone.service.user.UserService;
 import com.shopee.clone.util.ResponseObject;
 import org.modelmapper.ModelMapper;
@@ -20,8 +20,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 
@@ -34,58 +36,51 @@ public class CartServiceImpl implements CartService {
     private ModelMapper modelMapper;
     @Autowired
     private OptionTypeRepository optionType;
-    @Autowired
-    private OptionValueRepository optionValue;
+
     @Autowired
     private ProductItemRepository productItemRepository;
+    @Autowired
+    private ProductItemService productItemService;
     @Override
     public ResponseEntity<?> addToCart(Long pItemId, Long uId) {
         try{
             CartEntity cartEntity = new CartEntity();
             Optional<UserEntity> user = userService.findUserByID(uId);
             if (user.isPresent()) {
+                List<CartEntity> cartEntities = cartRepository.findByUser(user.get());
                 Optional<ProductItemEntity> productItem = productItemRepository.findById(pItemId);
+
+
                 if (productItem.isPresent()) {
-                    cartEntity.setUser(user.get());
-                    cartEntity.setProductItems(productItem.get());
-                    cartEntity.setQuantity(1);
-                    cartRepository.save(cartEntity);
+                    Long check = findCartItemId(cartEntities,productItem.get());
+                    if(check!=null) {
+                        increaseQty(check);
+                        cartEntities = cartRepository.findByUser(user.get());
+                        List<CartResponse> cartRepositories = converterCartResponses(cartEntities);
+                        return ResponseEntity.ok().body(ResponseObject
+                                .builder()
+                                .status("SUCCESS")
+                                .message("Add to cart success !")
+                                .results(cartRepositories)
+                                .build());
+                    }else {
+                        cartEntity.setUser(user.get());
+                        cartEntity.setProductItems(productItem.get());
+                        cartEntity.setQuantity(1);
+                        cartRepository.save(cartEntity);
 
-                    List<CartEntity> cartEntities = cartRepository.findByUser(user.get());
+                        cartEntities = cartRepository.findByUser(user.get());
+
+                        List<CartResponse> cartRepositories = converterCartResponses(cartEntities);
+                        return ResponseEntity.ok().body(ResponseObject
+                                .builder()
+                                .status("SUCCESS")
+                                .message("Add to cart success !")
+                                .results(cartRepositories)
+                                .build());
 
 
-
-                    List<CartResponse> cartRepositories =
-                            cartEntities
-                                    .stream()
-                                    .map(c -> {
-                                        CartResponse cartResponse=  new CartResponse(
-                                            modelMapper
-                                                    .map(c.getProductItems().getProduct(), ProductMatchToCartResponse.class)
-                                                     ,c.getQuantity());
-                                        ProductItemResponseDTO productItemDTO = modelMapper.map(c.getProductItems(), ProductItemResponseDTO.class);
-
-                                        List<OptionTypeDTO> typeList = new ArrayList<>();
-                                        c.getProductItems()
-                                                .getOptionValues()
-                                                .forEach(v->
-                                                {
-                                                    OptionTypeDTO type = modelMapper.map(v.getOptionType(),OptionTypeDTO.class);
-                                                    type.setOptionValue(modelMapper.map(v,OptionValueDTO.class));
-                                                    typeList.add(type);
-                                                });
-
-                                        cartResponse.getProduct().setProductItemResponse(productItemDTO);
-                                        cartResponse.getProduct().getProductItemResponse().setOptionTypes(typeList);
-
-                                        return cartResponse;
-                                    }).toList();
-                    return ResponseEntity.ok().body(ResponseObject
-                            .builder()
-                            .status("SUCCESS")
-                            .message("Add to cart success !")
-                            .results(cartRepositories)
-                            .build());
+                    }
                 }
                 return ResponseEntity
                         .badRequest()
@@ -117,9 +112,193 @@ public class CartServiceImpl implements CartService {
 
     }
 
+    private Long findCartItemId(List<CartEntity> cartEntities, ProductItemEntity productItemEntity) {
+        Long result = null;
+        for (CartEntity cartEntity : cartEntities) {
+            if (Objects.equals(cartEntity.getProductItems().getPItemId(), productItemEntity.getPItemId())) {
+                result = cartEntity.getId();
+                break; // Exit the loop as soon as a match is found
+            }
+        }
+        return result;
+    }
+
+    private List<CartResponse> converterCartResponses(List<CartEntity> cartEntities) {
+        List<CartResponse> cartResponse =
+                cartEntities
+                        .stream()
+                        .map(c -> {
+                            CartResponse cart=  new CartResponse(
+                                modelMapper
+                                        .map(c.getProductItems().getProduct(), ProductMatchToCartResponse.class)
+                                         ,c.getQuantity());
+                            ProductItemResponseDTO productItemDTO = modelMapper.map(c.getProductItems(), ProductItemResponseDTO.class);
+
+                            List<OptionTypeDTO> typeList = new ArrayList<>();
+                            c.getProductItems()
+                                    .getOptionValues()
+                                    .forEach(v->
+                                    {
+                                        OptionTypeDTO type = modelMapper.map(v.getOptionType(),OptionTypeDTO.class);
+                                        type.setOptionValue(modelMapper.map(v,OptionValueDTO.class));
+                                        typeList.add(type);
+                                    });
+
+                            cart.getProduct().setProductItemResponse(productItemDTO);
+                            cart.getProduct().getProductItemResponse().setOptionTypes(typeList);
+
+                            return cart;
+                        }).toList();
+        return cartResponse;
+    }
+
     @Override
     public void remove(Long id) {
-        Optional<CartEntity> cartEntity = cartRepository.findById(id);
-        cartRepository.delete(cartEntity.get());
+    }
+
+    @Override
+    public ResponseEntity<?> increaseQty(Long cartId) {
+        try{
+            Optional<CartEntity> cartOptional = cartRepository.findById(cartId);
+            if(cartOptional.isPresent()){
+                CartEntity cart = cartOptional.get();
+                boolean check = productItemService.checkAvailableQuantityInStock
+                        (cart.getProductItems().getPItemId(),cart.getQuantity());
+                if(check){
+                    cart.setQuantity(cart.getQuantity()+1);
+                    cartRepository.save(cart);
+
+                    List<CartEntity> cartList = cartRepository.findByUser(cart.getUser());
+
+                    List<CartResponse> cartRepositories = converterCartResponses(cartList);
+                    return ResponseEntity.ok().body(ResponseObject
+                            .builder()
+                            .status("SUCCESS")
+                            .message("Update quantity success !")
+                            .results(cartRepositories)
+                            .build());
+
+                }
+                return ResponseEntity
+                        .badRequest()
+                        .body(ResponseObject.builder()
+                                .status("FAIL")
+                                .message("Invalid quantity!")
+                                .results("")
+                                .build()
+                        );
+
+            }
+            return ResponseEntity
+                    .badRequest()
+                    .body(ResponseObject.builder()
+                            .status("FAIL")
+                            .message("CartId not found!")
+                            .results("")
+                            .build()
+                    );
+        }catch (Exception e){
+            return ResponseEntity
+                    .badRequest()
+                    .body(ResponseObject.builder()
+                            .status("FAIL")
+                            .message(e.getMessage())
+                            .results("")
+                            .build()
+                    );
+        }
+        
+    }
+
+    @Override
+    public ResponseEntity<?> reduceQty(Long cartId) {
+        try{
+            Optional<CartEntity> cartOptional = cartRepository.findById(cartId);
+            if(cartOptional.isPresent()){
+                CartEntity cart = cartOptional.get();
+                boolean check = cart.getQuantity()>=1;
+                if(check){
+                    cart.setQuantity(cart.getQuantity()-1);
+                    cartRepository.save(cart);
+
+                    List<CartEntity> cartList = cartRepository.findByUser(cart.getUser());
+
+                    List<CartResponse> cartRepositories = converterCartResponses(cartList);
+                    return ResponseEntity.ok().body(ResponseObject
+                            .builder()
+                            .status("SUCCESS")
+                            .message("Update quantity success !")
+                            .results(cartRepositories)
+                            .build());
+
+                }
+                return ResponseEntity
+                        .badRequest()
+                        .body(ResponseObject.builder()
+                                .status("FAIL")
+                                .message("Invalid quantity!")
+                                .results("")
+                                .build()
+                        );
+
+            }
+            return ResponseEntity
+                    .badRequest()
+                    .body(ResponseObject.builder()
+                            .status("FAIL")
+                            .message("CartId not found!")
+                            .results("")
+                            .build()
+                    );
+        }catch (Exception e){
+            return ResponseEntity
+                    .badRequest()
+                    .body(ResponseObject.builder()
+                            .status("FAIL")
+                            .message(e.getMessage())
+                            .results("")
+                            .build()
+                    );
+        }
+
+    }
+
+    @Override
+    public ResponseEntity<?> delete(Long cartId) {
+        try{
+            Optional<CartEntity> cartOptional = cartRepository.findById(cartId);
+            if(cartOptional.isPresent()){
+                CartEntity cart = cartOptional.get();
+
+                    cartRepository.delete(cart);
+
+                    List<CartEntity> cartList = cartRepository.findByUser(cart.getUser());
+
+                    List<CartResponse> cartRepositories = converterCartResponses(cartList);
+                    return ResponseEntity.ok().body(ResponseObject
+                            .builder()
+                            .status("SUCCESS")
+                            .message("Delete product item in cart success !")
+                            .results(cartRepositories)
+                            .build());
+            }
+            return ResponseEntity
+                    .badRequest()
+                    .body(ResponseObject.builder()
+                            .status("FAIL")
+                            .message("CartId not found!")
+                            .results("")
+                            .build()
+                    );
+        }catch (Exception e){
+            return ResponseEntity
+                    .badRequest()
+                    .body(ResponseObject.builder()
+                            .status("FAIL")
+                            .message(e.getMessage())
+                            .results("")
+                            .build()
+                    );
+        }
     }
 }
