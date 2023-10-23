b8e2508d44224984c199ca0980df19bab06844be
Thai Hung <thaihungnguyenabh@gmail.com>
2023-10-23 16:05:25 +0700

    fix function check promotion available: add condition check promotion on store(By sellerId)

--- clone/src/main/java/com/shopee/clone/service/promotion/IPromotionService.java
+++ clone/src/main/java/com/shopee/clone/service/promotion/IPromotionService.java
@@ -10,12 +10,12 @@ import java.util.List;
 public interface IPromotionService {
     ResponseEntity<?> createPromotion(PromotionRequestCreate promotionRequestCreate);
     ResponseEntity<?> editStatusPromotion(Long promotionId, Boolean status);
-    Boolean isValidPromotion(String name, Double purchasedAmount);
+    Boolean isValidPromotion(String name, Double purchasedAmount, Long seller_id);
     ResponseEntity<?> addPromotionBeLongUser(Long userId, List<Long> listPromotionId);
     //Call To this Service is Enough
-    Boolean checkValidUsage(Long userId, String promotionName, Double purchasedAmount);
-    Boolean minusUsage(Long userId, String promotionName, Double purchasedAmount);
-    Boolean plusUsage(Long userId, String promotionName, Double purchasedAmount);
+    Boolean checkValidUsage(Long userId, String promotionName, Double purchasedAmount, Long seller_id);
+    Boolean minusUsage(Long userId, String promotionName, Double purchasedAmount, Long seller_id);
+    Boolean plusUsage(Long userId, String promotionName, Double purchasedAmount, Long seller_id);
     ResponseEntity<?> getAllPromotionAvailable(Long userId);
     ResponseEntity<?> getAllPromotionBySellerId(Long sellerId);
     ResponseEntity<?> getPromotionOfUser(Long userId);
