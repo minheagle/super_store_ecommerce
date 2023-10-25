73307748f2b4fd8815bd5ca01d43c0ac6d2f10e2
MinhEagle <hoangnhatminh23081998@gmail.com>
2023-10-25 15:43:53 +0700

    change order

--- clone/src/main/java/com/shopee/clone/DTO/seller/SellerDTO.java
+++ clone/src/main/java/com/shopee/clone/DTO/seller/SellerDTO.java
@@ -14,8 +14,13 @@ public class SellerDTO {
     private String storeName;
     private Long userId;
     private String storeAddress;
+    private String imgAvatarPublicId;
     private String storeAvatarUrl;
+    private String imgBackgroundPublicId;
     private String storeBackgroundUrl;
+    private String storePhoneNumber;
+    private String storeBankName;
+    private String storeBankAccountNumber;
     private Date createdAt;
     private Integer numberFollower;
 }
--- clone/src/main/java/com/shopee/clone/service/order/impl/OrderServiceImpl.java
+++ clone/src/main/java/com/shopee/clone/service/order/impl/OrderServiceImpl.java
@@ -78,11 +78,10 @@ public class OrderServiceImpl implements OrderService {
             Optional<AddressEntity> addressOptional = addressRepository.findById(orderRequest.getAddressId());
 
             if(userOptional.isPresent() && addressOptional.isPresent()){
-                int finalOrderNumber = getFinalOrderNumber();
-
 //              Chạy vòng lặp để lưu các đơn hàng theo từng shop
 
                 orderRequest.getListOrderBelongToSeller().forEach(o ->{
+                    int finalOrderNumber = getFinalOrderNumber();
                     UserEntity user = userOptional.get();
                     AddressEntity address = addressOptional.get();
 
@@ -360,7 +359,7 @@ public class OrderServiceImpl implements OrderService {
     public ResponseEntity<?> cancelOrder(Long orderId) {
         try {
             Optional<OrderEntity> orderEntity = orderRepository.findById(orderId);
-
+            orderEntity.get().getId();
             if(orderEntity.isPresent()){
                 OrderEntity order = orderEntity.get();
                 if(order.getStatus().equals(EOrder.Pending) || order.getStatus().equals(EOrder.Awaiting_Payment)|| order.getStatus().equals(EOrder.Transferred)){
